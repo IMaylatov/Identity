939a5adce0e112f2aa8effcb7ab0fdfe6830b655
Майлатов Иван <imaylatov@softrust.ru>
2020-12-13 14:08:40 +0300

    #skipchecks refactoring

--- Config.cs
+++ Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4;
+﻿using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -11,12 +12,14 @@ namespace SofTrust.IdentityServer
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
+                new IdentityResource("roles", new[] { "role" })
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
-                new ApiScope("softrust_report_api", "SofTrust Report API")
+                new ApiScope("softrust_report_api", "SofTrust Report API", new List<string>() { "role" }),
+                new ApiScope("softrust_account_api", "SofTrust Account API", new List<string>() { "role" })
             };
 
         public static IEnumerable<Client> Clients =>
@@ -27,11 +30,11 @@ namespace SofTrust.IdentityServer
                     ClientId = "softrust_report_ui",
                     ClientName = "SofTrust Report UI",
                     ClientUri = "http://localhost:3000",
-
+                    
                     AllowedGrantTypes = GrantTypes.Implicit,
 
                     RequireClientSecret = false,
-
+                    
                     RedirectUris = { "http://localhost:3000/signin-oidc", "http://localhost:3000/silentrenew" },
 
                     PostLogoutRedirectUris = { "http://localhost:3000/logout/callback" },
@@ -42,7 +45,36 @@ namespace SofTrust.IdentityServer
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
+                        JwtClaimTypes.Role,
                         "softrust_report_api"
+                    },
+
+                    AllowAccessTokensViaBrowser = true,
+
+                    AllowOfflineAccess = true
+                },
+                new Client
+                {
+                    ClientId = "softrust_account_ui",
+                    ClientName = "SofTrust Account UI",
+                    ClientUri = "http://localhost:3001",
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    RequireClientSecret = false,
+
+                    RedirectUris = { "http://localhost:3001/signin-oidc", "http://localhost:3001/silentrenew" },
+
+                    PostLogoutRedirectUris = { "http://localhost:3001/logout/callback" },
+
+                    AllowedCorsOrigins = { "http://localhost:3001" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        JwtClaimTypes.Role,
+                        "softrust_account_api"
                     },
 
                     AllowAccessTokensViaBrowser = true,
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
